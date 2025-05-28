d96a1c5f2403b658587ae06a0ad57f3e23a482f0
Amirali <amiraliesmaeli741@gmail.com>
2025-05-28 21:50:44 +0330

    Add functions for generating initial population with 5 ones in 10-bit binary strings

---
+++ genetic_solar_optimization/AI_Project_Part_2.py
@@ -0,0 +1,11 @@
+import random
+
+def generate_individual():
+    individual = [0] * 10
+    indices = random.sample(range(10), 5)
+    for i in indices:
+        individual[i] = 1
+    return individual
+
+def generate_population(size):
+    return [generate_individual() for _ in range(size)]
